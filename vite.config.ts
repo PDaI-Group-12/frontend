29d3b4d6ab70fe1a175a14530e4b6f411a0d68e3
syorito-hatsuki <enderdron3@gmail.com>
2024-10-29 12:30:19 +0200

    Fixed deploy URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: 'https://pdai-group-12.github.io/',
+  base: 'https://pdai-group-12.github.io/frontend',
     plugins: [react()],
 })
