f7c64d7c29586edb3763cec09f61be7274fc7974
syorito-hatsuki <enderdron3@gmail.com>
2024-11-05 18:44:39 +0200

    Added login form

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,14 @@
 import {useAuth} from "../hooks/useAuth.tsx";
 import {useNavigate} from "react-router-dom";
-import {TextField, Typography} from "@mui/material";
+import {
+    Button,
+    Card,
+    Container,
+    FormControl,
+    Stack,
+    TextField,
+    Typography
+} from "@mui/material";
 
 export function LoginPage() {
 
@@ -10,9 +18,25 @@ export function LoginPage() {
     if (isAuthorized) navigate("/home")
 
     return (
-        <>
-            <Typography variant="h2">PDaI</Typography>
-            <TextField label="Login" type="text"/>
-        </>
+        <Container maxWidth="xs">
+            <Typography variant="h4" align="center" paddingBottom={2}>Log In</Typography>
+            <Card>
+                <form>
+                    <FormControl fullWidth>
+                        <Stack padding={2} spacing={2}>
+                            <TextField label="Username" variant="outlined" type="text" required onChange={() => {
+                                // Capture login
+                            }}/>
+                            <TextField label="Password" variant="outlined" type="password" required onChange={() => {
+                                // Capture password
+                            }}/>
+                            <Button type="submit" variant="contained" fullWidth onClick={() => {
+                                // Authorize
+                            }}>Login</Button>
+                        </Stack>
+                    </FormControl>
+                </form>
+            </Card>
+        </Container>
     )
 }
