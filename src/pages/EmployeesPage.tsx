93a354dcd2e5bf707c7ad1c31ae876bb8e0bec4f
Deforested <Teemuyritys@gmail.com>
2024-11-06 12:52:51 +0200

    Created and added routing to Employees page. Made some dummy content to visualise the page.

---
+++ src/pages/EmployeesPage.tsx
@@ -0,0 +1,43 @@
+import {Avatar, Box, List, ListItem, ListItemAvatar, ListItemText, Typography} from "@mui/material";
+
+
+type Employee = {
+    id: number,
+    fname: string,
+    lname: string,
+    iban: string
+}
+
+export function EmployeesPage() {
+    const sampleEmployees:Employee[] = [
+        {id:1, fname:"John", lname: "Doe", iban: "FI 1234 123 12"},
+        {id:2, fname:"Mary", lname: "Poppins", iban: "FI 4321 321 21"},
+        {id:3, fname:"Harry", lname: "Potter", iban: "FI 3456 456 56"}
+    ];
+    return (
+        <Box
+            p={2}
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+        >
+            <Typography variant="h4">
+                Employees
+            </Typography>
+            <List>
+                {sampleEmployees.map((employee) => (
+                    <ListItem key={employee.id}>
+                        <ListItemAvatar>
+                            <Avatar>{employee.fname[0]}</Avatar>
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary={`${employee.fname} ${employee.lname}`}
+                            secondary={`IBAN: ${employee.iban}`}
+                        />
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
+}
