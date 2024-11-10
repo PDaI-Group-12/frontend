8f889916ca08868e3ac478918cade7734032fd8b
syorito-hatsuki <enderdron3@gmail.com>
2024-11-10 23:56:26 +0200

    Replaced the list with Card Stack

--- src/pages/EmployeesPage.tsx
+++ src/pages/EmployeesPage.tsx
@@ -1,37 +1,41 @@
-import {Avatar, Box, List, ListItem, ListItemAvatar, ListItemText, Typography} from "@mui/material";
+import {
+    Avatar,
+    Card,
+    CardActionArea,
+    CardContent,
+    Container,
+    Stack,
+    Typography
+} from "@mui/material";
 import {Employee} from "./types.ts";
 
 
 export function EmployeesPage() {
-    const sampleEmployees:Employee[] = [
-        {id:1, fname:"John", lname: "Doe", iban: "FI 1234 123 12"},
-        {id:2, fname:"Mary", lname: "Poppins", iban: "FI 4321 321 21"},
-        {id:3, fname:"Harry", lname: "Potter", iban: "FI 3456 456 56"}
+    const sampleEmployees: Employee[] = [
+        {id: 1, fname: "John", lname: "Doe", iban: "FI 1234 123 12"},
+        {id: 2, fname: "Mary", lname: "Poppins", iban: "FI 4321 321 21"},
+        {id: 3, fname: "Harry", lname: "Potter", iban: "FI 3456 456 56"}
     ];
     return (
-        <Box
-            p={2}
-            display="flex"
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="center"
-        >
-            <Typography variant="h4">
-                Employees
-            </Typography>
-            <List>
+        <Container maxWidth="xs" style={{justifyContent: "center", width: "max-content"}}>
+            <Typography variant="h4" align="center" paddingBottom={2}>Employees</Typography>
+            <Stack spacing={2}>
                 {sampleEmployees.map((employee) => (
-                    <ListItem key={employee.id}>
-                        <ListItemAvatar>
-                            <Avatar>{employee.fname[0]}</Avatar>
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary={`${employee.fname} ${employee.lname}`}
-                            secondary={`IBAN: ${employee.iban}`}
-                        />
-                    </ListItem>
+                    <Card key={employee.id}>
+                        <CardActionArea>
+                            <CardContent>
+                                <Stack direction="row" spacing={2} alignItems="center">
+                                    <Avatar>{employee.fname[0]}{employee.lname[0]}</Avatar>
+                                    <Stack>
+                                        <Typography>{employee.fname} {employee.lname}</Typography>
+                                        <Typography color="darkgray">IBAN: {employee.iban}</Typography>
+                                    </Stack>
+                                </Stack>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
                 ))}
-            </List>
-        </Box>
+            </Stack>
+        </Container>
     );
 }
