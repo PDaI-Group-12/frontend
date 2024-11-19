8963d9f7c5b7318268f73beb14e948a09e0458fe
syorito-hatsuki <enderdron3@gmail.com>
2024-11-20 01:55:17 +0200

    Fixed fab position and added some checks

--- src/pages/EditUserPage.tsx
+++ src/pages/EditUserPage.tsx
@@ -1,93 +1,128 @@
 import {Card, Container, Fab, FormControl, Stack, TextField, Typography} from "@mui/material";
 import {Employee} from "./types.ts";
 import {Edit, Save} from "@mui/icons-material";
-import {useState} from "react";
-
+import {CSSProperties, FormEvent, useState} from "react";
 
 export function EditUserPage() {
-    const exampleEmployee: Employee = {id: 1, fname: "John", lname: "Doe", iban: "FI 1234 123 12", role: "worker"}
 
+    const exampleEmployee: Employee = {
+        id: 1,
+        fname: "John",
+        lname: "Doe",
+        iban: "FI 1234 123 12",
+        role: "worker"
+    }
+
+    const [firstName, setFirstName] = useState(exampleEmployee.fname)
+    const [lastName, setLastName] = useState(exampleEmployee.lname)
+    const [password, setPassword] = useState("")
+    const [iban, setIBAN] = useState(exampleEmployee.iban)
     const [isEdit, setIsEdit] = useState(false);
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault(); // Prevent page reload
-        // Process form data here
-        console.log("Form submitted");
-        setIsEdit(false); // Exit edit mode
-    };
+    // TODO Extract with typography into custom component
+    const worldBreaker: CSSProperties = {
+        wordBreak: "break-word"
+    }
+
+    //TODO Later extract checks to other file to share them between login, edit and else
+
+    const isPasswordInvalid = (): boolean => {
+        //TODO Replace it with regex later after meeting with a team
+        return !(password.length > 0)
+    }
+
+    const isFirstNameInvalid = (): boolean => {
+        //TODO Replace it with regex later after meeting with a team
+        return !(firstName.length > 0)
+    }
+
+    const isLastNameInvalid = (): boolean => {
+        //TODO Replace it with regex later after meeting with a team
+        return !(lastName.length > 0)
+    }
+
+    const isIBANInvalid = (): boolean => !/^[A-Z]{2}\d{2}[A-Z0-9]{1,30}$/.test(iban.replace(/\s+/g, ''));
 
     return (
         <Container maxWidth="xs">
-            <Typography variant="h5" textAlign="center">Profile</Typography>
-            {isEdit ? (
-                <Card>
-                    <form onSubmit={handleSubmit}>
-                        <FormControl>
-                            <Stack padding={2} spacing={2}>
-                                <TextField
-                                    label="First Name"
-                                    required
-                                    placeholder={exampleEmployee.fname}
-                                    variant="standard"
-                                    type="text"
-                                    defaultValue={exampleEmployee.fname}
-                                />
-                                <TextField
-                                    label="Last Name"
-                                    required
-                                    placeholder={exampleEmployee.lname}
-                                    variant="standard"
-                                    type="text"
-                                    defaultValue={exampleEmployee.lname}
-                                />
-                                <TextField
-                                    label="Password"
-                                    required
-                                    placeholder="Dont use qwerty"
-                                    variant="standard"
-                                    type="password"
-                                />
-                                <TextField
-                                    label="IBAN"
-                                    required
-                                    placeholder={exampleEmployee.iban}
-                                    variant="standard"
-                                    type="text"
-                                    defaultValue={exampleEmployee.iban}
-                                />
-                                <Fab
-                                    type="submit"
-                                    sx={{alignSelf: "center", marginTop: 2}}
-                                    size="medium"
-                                    color="secondary"
-                                >
-                                    <Save />
-                                </Fab>
-                            </Stack>
-                        </FormControl>
-                    </form>
-                </Card>
-            ) : (
-                <Card>
-                    <Stack padding={2} spacing={2}>
-                        <Typography style={{wordBreak: "break-word"}}>First Name: {exampleEmployee.fname}</Typography>
-                        <Typography style={{wordBreak: "break-word"}}>Last Name: {exampleEmployee.lname}</Typography>
-                        <Typography style={{wordBreak: "break-word"}}>Password: *********</Typography>
-                        <Typography style={{wordBreak: "break-word"}}>IBAN: {exampleEmployee.iban}</Typography>
-                    </Stack>
-                </Card>
-            )}
-            {!isEdit && ( // Only show the Fab button when not in edit mode
-                <Fab
-                    onClick={() => setIsEdit(prevState => !prevState)}
-                    sx={{position: "absolute", bottom: 16, right: 16}}
-                    size="medium"
-                    color="secondary"
-                >
-                    <Edit />
-                </Fab>
-            )}
-        </Container>
-    );
-}
+            <Typography variant="h4" textAlign="center" paddingBottom={2}>Profile</Typography>
+            <Card>
+                <form onSubmit={(event: FormEvent) => {
+                    event.preventDefault()
 
+                    if (isFirstNameInvalid() || isLastNameInvalid() || isPasswordInvalid() || isIBANInvalid()) return
+
+                    setIsEdit(false)
+                    setPassword("") //TODO Remove later while add integration with backend
+                }}>
+                    <FormControl fullWidth>
+                        <Stack padding={2} spacing={2}>
+                            {
+                                isEdit ? <>
+                                    <TextField
+                                        label="First Name"
+                                        //TODO Remove if backend uses PATCH, else left unchanged
+                                        required
+                                        error={isFirstNameInvalid()}
+                                        placeholder={exampleEmployee.fname}
+                                        variant="standard"
+                                        type="text"
+                                        value={firstName}
+                                        onChange={event => setFirstName(event.target.value.trim())}
+                                    />
+                                    <TextField
+                                        label="Last Name"
+                                        //TODO Remove if backend uses PATCH, else left unchanged
+                                        required
+                                        error={isLastNameInvalid()}
+                                        placeholder={exampleEmployee.lname}
+                                        variant="standard"
+                                        type="text"
+                                        value={lastName}
+                                        onChange={event => setLastName(event.target.value.trim())}
+                                    />
+                                    <TextField
+                                        label="Password"
+                                        //TODO Remove if backend uses PATCH, else left unchanged
+                                        required
+                                        error={isPasswordInvalid()}
+                                        placeholder="Dont use qwerty"
+                                        variant="standard"
+                                        type="password"
+                                        value={password}
+                                        onChange={event => setPassword(event.target.value.trim())}
+                                    />
+                                    <TextField
+                                        label="IBAN"
+                                        //TODO Remove if backend uses PATCH, else left unchanged
+                                        required
+                                        error={isIBANInvalid()}
+                                        placeholder={exampleEmployee.iban}
+                                        variant="standard"
+                                        type="text"
+                                        value={iban}
+                                        onChange={event => setIBAN(event.target.value.trim())}
+                                    />
+                                </> : <>
+                                    <Typography style={worldBreaker}><b>First Name:</b> {firstName}</Typography>
+                                    <Typography style={worldBreaker}><b>Last Name:</b> {lastName}</Typography>
+                                    <Typography style={worldBreaker}><b>Password:</b> *********</Typography>
+                                    <Typography style={worldBreaker}><b>IBAN:</b> {iban}</Typography>
+                                </>
+                            }
+                        </Stack>
+                        <Fab
+                            onClick={() => !isEdit && setIsEdit(true)}
+                            type="submit"
+                            sx={{position: "fixed", bottom: 16, right: 16}}
+                            size="medium"
+                            color="secondary"
+                        >
+                            {isEdit ? <Save/> : <Edit/>}
+                        </Fab>
+                    </FormControl>
+                </form>
+            </Card>
+        </Container>
+    )
+}
