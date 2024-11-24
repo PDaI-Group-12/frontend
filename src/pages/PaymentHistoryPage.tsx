d62f7517221a56574b656f2a6af6bd208552a3b4
Deforested <Teemuyritys@gmail.com>
2024-11-24 10:49:17 +0200

    Made a mockup version of the proper table.

--- src/pages/PaymentHistoryPage.tsx
+++ src/pages/PaymentHistoryPage.tsx
@@ -1,6 +1,20 @@
-import {Paper, Stack, Table, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import {Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+
 
 export function PaymentHistoryPage ()  {
+
+    function createData(
+        name: string,
+        payment: number,
+        date: string
+    ) {
+        return {name, payment, date}
+    }
+
+    const rows = [
+        createData("Antti Korhonen",2000,"20.02.2020"),
+        createData("Matti Latvala",1500,"16.06.2020")
+    ]
 
     return (
         <Stack padding={2} spacing={2} sx={{alignItems:"center"}}>
@@ -8,11 +22,20 @@ export function PaymentHistoryPage ()  {
                 <Table>
                     <TableHead>
                         <TableRow>
-                            <TableCell>Workers name</TableCell>
+                            <TableCell>Payers name</TableCell>
                             <TableCell align="right">Payment(€)</TableCell>
                             <TableCell align="right">Date</TableCell>
                         </TableRow>
                     </TableHead>
+                    <TableBody>
+                        {rows.map((row) => (
+                            <TableRow key={row.name}>
+                                <TableCell> {row.name} </TableCell>
+                                <TableCell align="right" > {row.payment} </TableCell>
+                                <TableCell align="right" > {row.date} </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
                 </Table>
             </TableContainer>
         </Stack>
