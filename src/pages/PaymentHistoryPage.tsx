b6c507ae34adb8be1d37ef8f1518f71722b06801
Deforested <Teemuyritys@gmail.com>
2024-11-25 13:19:43 +0200

    Replaced table with card.

--- src/pages/PaymentHistoryPage.tsx
+++ src/pages/PaymentHistoryPage.tsx
@@ -1,43 +1,62 @@
-import {Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import {Avatar, Card, CardContent, Container, Stack, Typography} from "@mui/material";
 
 
-export function PaymentHistoryPage ()  {
+export function PaymentHistoryPage() {
 
-    function createData(
-        name: string,
-        payment: number,
-        date: string
-    ) {
-        return {name, payment, date}
-    }
-
-    const rows = [
-        createData("Antti Korhonen",2000,"20.02.2020"),
-        createData("Matti Latvala",1500,"16.06.2020")
-    ]
 
     return (
-        <Stack padding={2} spacing={2} sx={{alignItems:"center"}}>
-            <TableContainer component={Paper} sx={{width: 800}}>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Payers name</TableCell>
-                            <TableCell align="right">Payment(€)</TableCell>
-                            <TableCell align="right">Date</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map((row) => (
-                            <TableRow key={row.name}>
-                                <TableCell> {row.name} </TableCell>
-                                <TableCell align="right" > {row.payment} </TableCell>
-                                <TableCell align="right" > {row.date} </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </Stack>
+        <Container maxWidth="xs">
+            <Typography variant="h4" align="center">Payment History</Typography>
+            <Stack padding={2} spacing={2}>
+                <Card>
+                    <CardContent>
+                        <Stack spacing={2} direction="row" justifyContent="space-between" alignItems="center">
+                            <Avatar>MM</Avatar>
+                            <Typography variant="h6">Matti Meikäläinen</Typography>
+                            <Stack alignItems="end">
+                                <Typography variant="h6">16.06.2020</Typography>
+                                <Typography color="gray" fontWeight="bold" variant="h6">2000€</Typography>
+                            </Stack>
+                        </Stack>
+                    </CardContent>
+                </Card>
+                <Card>
+                    <CardContent>
+                        <Stack spacing={2} direction="row" justifyContent="space-between" alignItems="center">
+                            <Avatar>MM</Avatar>
+                            <Typography variant="h6">Matti Meikäläinen</Typography>
+                            <Stack alignItems="end">
+                                <Typography variant="h6">16.06.2020</Typography>
+                                <Typography color="gray" fontWeight="bold" variant="h6">2000€</Typography>
+                            </Stack>
+                        </Stack>
+                    </CardContent>
+                </Card>
+                <Card>
+                    <CardContent>
+                        <Stack spacing={2} direction="row" justifyContent="space-between" alignItems="center">
+                            <Avatar>MM</Avatar>
+                            <Typography variant="h6">Matti Meikäläinen</Typography>
+                            <Stack alignItems="end">
+                                <Typography variant="h6">16.06.2020</Typography>
+                                <Typography color="gray" fontWeight="bold" variant="h6">2000€</Typography>
+                            </Stack>
+                        </Stack>
+                    </CardContent>
+                </Card>
+                <Card>
+                    <CardContent>
+                        <Stack spacing={2} direction="row" justifyContent="space-between" alignItems="center">
+                            <Avatar>MM</Avatar>
+                            <Typography variant="h6">Matti Meikäläinen</Typography>
+                            <Stack alignItems="end">
+                                <Typography variant="h6">16.06.2020</Typography>
+                                <Typography color="gray" fontWeight="bold" variant="h6">2000€</Typography>
+                            </Stack>
+                        </Stack>
+                    </CardContent>
+                </Card>
+            </Stack>
+        </Container>
     )
 }
